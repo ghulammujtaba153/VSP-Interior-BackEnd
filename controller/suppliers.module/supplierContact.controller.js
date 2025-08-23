fef332a1a251be5465cd03f14cc6bcdf6c261379
Mujtaba12-al <mujtaba@mativeinc.com>
2025-08-23 18:16:43 +0500

    commit

--- controller/suppliers.module/supplierContact.controller.js
+++ controller/suppliers.module/supplierContact.controller.js
@@ -6,7 +6,7 @@ export const createSupplierContact = async (req, res) => {
 
     try {
         const supplierContact = await SupplierContacts.create(req.body);
-        await Audit.create({ userId: req.body.userId, action: 'create', tableName: 'supplierContacts', newData: supplierContact.get() });
+        await Audit.create({ userId: req.body.userId, action: 'create', tableName: 'supplierContacts', newData: supplierContact});
         res.status(201).json({
             message: "Supplier contact created successfully",
             data: supplierContact
@@ -41,7 +41,7 @@ export const updateSupplierContact = async (req, res) => {
         const supplierContact = await SupplierContacts.update(req.body, {
             where: { id: req.params.id }
         });
-        await Audit.create({ userId: req.body.userId, action: 'update', tableName: 'supplierContacts', oldData: supplierContact.get(), newData: req.body });
+        await Audit.create({ userId: req.body.userId, action: 'update', tableName: 'supplierContacts', oldData: supplierContact, newData: req.body });
         res.status(200).json({
             message: "Supplier contact updated successfully",
             data: supplierContact
@@ -60,7 +60,7 @@ export const deleteSupplierContact = async (req, res) => {
         const supplierContact = await SupplierContacts.destroy({
             where: { id: req.params.id }
         });
-        await Audit.create({ userId: req.body.userId, action: 'delete', tableName: 'supplierContacts', oldData: supplierContact.get() });
+        await Audit.create({ userId: req.body.userId, action: 'delete', tableName: 'supplierContacts', oldData: supplierContact });
         res.status(200).json({
             message: "Supplier contact deleted successfully",
             data: supplierContact

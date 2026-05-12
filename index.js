25a146d4abd640f6a93695dd518d0ee412aa1fd5
Mujtaba12-al <mujtaba@mativeinc.com>
2026-05-12 13:03:35 +0500

    cors options updated

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
+
+app.options("(.*)", cors(corsOptions));
 
 
 app.use("/uploads", express.static("uploads"));
